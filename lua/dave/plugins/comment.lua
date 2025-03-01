637569426cc0a69721d1a3179e6fb9edb637fe12
ddaoxuan <dawid.daoxuan.uk@gmail.com>
2025-03-01 02:33:55 +0100

    tsx comments

--- lua/dave/plugins/comment.lua
+++ lua/dave/plugins/comment.lua
@@ -1,6 +1,11 @@
 return {
     'numToStr/Comment.nvim',
-    opts = {
-        -- add any options here
-    },
+    dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
+    config = function()
+        local tsx_comment =
+            require('ts_context_commentstring.integrations.comment_nvim')
+        require('Comment').setup({
+            pre_hook = tsx_comment.create_pre_hook(),
+        })
+    end,
 }
