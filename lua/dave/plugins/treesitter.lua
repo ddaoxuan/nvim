f66eb7ca7963ed6da7a815f4b3986758256012be
ddaoxuan <dawid.daoxuan.uk@gmail.com>
2025-04-11 00:42:02 +0200

    treesitter function block support

--- lua/dave/plugins/treesitter.lua
+++ lua/dave/plugins/treesitter.lua
@@ -1,6 +1,9 @@
 return {
     -- Syntax Highlight, edit, and navigate code % etc
     'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+    },
     build = ':TSUpdate', -- treesitter update not typescript update ..
     config = function()
         require('nvim-treesitter.configs').setup({
@@ -16,6 +19,40 @@ return {
                 'vimdoc',
                 'vim',
                 'jsdoc',
+            },
+
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+                    keymaps = {
+                        -- You can use the capture groups defined in textobjects.scm
+                        ['af'] = '@function.outer',
+                        ['if'] = '@function.inner',
+                        ['ac'] = '@class.outer',
+                        ['ic'] = '@class.inner',
+                    },
+                },
+                move = {
+                    enable = true,
+                    set_jumps = true, -- whether to set jumps in the jumplist
+                    goto_next_start = {
+                        [']f'] = '@function.outer',
+                        [']c'] = '@class.outer',
+                    },
+                    goto_next_end = {
+                        [']F'] = '@function.outer',
+                        [']C'] = '@class.outer',
+                    },
+                    goto_previous_start = {
+                        ['[f'] = '@function.outer',
+                        ['[c'] = '@class.outer',
+                    },
+                    goto_previous_end = {
+                        ['[F'] = '@function.outer',
+                        ['[C'] = '@class.outer',
+                    },
+                },
             },
 
             sync_install = false,
